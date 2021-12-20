066124322e2bbde954a14bed48f0b7da54a0eb77
torimusi <torimusiy@gmail.com>
2021-12-20 17:19:32 +0900

    create simple topology

--- simple_topo.py
+++ simple_topo.py
@@ -19,31 +19,17 @@ class MyTopo( Topo ):
         "Create custom topo."
 
         # Add hosts and switches
-        host1 = self.addHost( 'h1' )
-        host2 = self.addHost( 'h2' )
-        host3 = self.addHost( 'h3' )
-        host4 = self.addHost( 'h4' )
-        host5 = self.addHost( 'h5' )
-        host6 = self.addHost( 'h6' )
+        gateway1 = self.addSwitch( 'g1' )
+        gateway2 = self.addSwitch( 'g2' )
+
         serv = self.addHost(' sv ')
         
-        Switch1 = self.addSwitch( 's1' )
-        Switch2 = self.addSwitch( 's2' )
-        Switch3 = self.addSwitch( 's3' )
-
-        # Add links
-        self.addLink( host1, Switch1 )
-        self.addLink( host2, Switch1 )
-        self.addLink( host3, Switch1 )
-
-        self.addLink( host4, Switch2 )
-        self.addLink( host5, Switch2 )
-        self.addLink( host6, Switch2 )
+        Switch = self.addSwitch( 's1' )
         
-        self.addLink( Switch1, Switch2 )
-        self.addLink( Switch2, Switch3 )
-        self.addLink( Switch3, Switch1 )
-
-        self.addLink( Switch3, serv )
+        # Add links
+        self.addLink( gateway1, Switch )
+        self.addLink( gateway2, Switch )
+        
+        self.addLink( Switch, serv )
         
 topos = { 'mytopo': ( lambda: MyTopo() ) }
