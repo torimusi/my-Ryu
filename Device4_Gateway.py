3863e6fd6135e0b8541a9958927f272ecd38ef55
torimusi <torimusiy@gmail.com>
2022-01-20 19:51:34 +0900

    def variable

--- Device4_Gateway.py
+++ Device4_Gateway.py
@@ -1,6 +1,7 @@
 # 4つのデバイスと外部ルータをつなぐゲートウェイ
 
 from distutils import command
+from termios import TOSTOP
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
@@ -8,6 +9,33 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+
+# アドミッション制御に用いる変数を定義
+QOS_FLAG      = 'flag'
+QOS_OFF       = 0
+QOS_ON        = 1
+QOS_IDLE_TIME = 120
+QOS_PRIORITY  = 65535
+
+TRAFFIC = 'traffic'
+PRIORITY_1_TRAFFIC = 100 * (10 ** 6)
+PRIORITY_2_TRAFFIC = 300 * (10 ** 6)
+PRIORITY_3_TRAFFIC = 300 * (10 ** 6)
+PRIORITY_4_TRAFFIC = 100 * (10 ** 6)
+
+TOS            = 'tos'
+PRIORITY_1_TOS = 32
+PRIORITY_2_TOS = 16
+PRIORITY_3_TOS = 8
+PRIORITY_4_TOS = 4
+
+ROUTER_MAC = '00:00:00:00:00:01'
+ROUTER_PORT = 1
+
+PRIORITY_1_PORT = 2
+PRIORITY_2_PORT = 3
+PRIORITY_3_PORT = 4
+PRIORITY_4_PORT = 5
 
 # クラスの定義
 class Device4Gateway(app_manager.RyuApp):
@@ -45,14 +73,14 @@ class Device4Gateway(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        self.add_flow(datapath, priority, match, actions)
+        self.add_flow(datapath, port)
 
     # フロー登録
     # 各ポートに各優先度のデバイスが接続
     # 送信先は外部ルータ
-    def add_flow(self, datapath, in_port, src, dst, actions):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+    def add_flow(self, port):
+        ofproto = self.datapath.ofproto
+        parser = self.datapath.ofproto_parser
 
         # ポートを見てフロー登録、送信先はルータ
         match = parser.OFPMatch(in_port=in_port, dl_src=src, dl_dst=dst)
