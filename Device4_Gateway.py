0bf166963e8fc77581febe526b69ecfe634d88db
torimusi <torimusiy@gmail.com>
2022-02-03 16:50:06 +0900

    update Device4_Gateway.py

--- Device4_Gateway.py
+++ Device4_Gateway.py
@@ -20,10 +20,10 @@ QOS_IDLE_TIME = 0
 QOS_PRIORITY  = 65535
 
 TRAFFIC = 'traffic'
-PRIORITY_1_TRAFFIC = 100
-PRIORITY_2_TRAFFIC = 300
-PRIORITY_3_TRAFFIC = 200
-PRIORITY_4_TRAFFIC = 100
+PRIORITY_1_TRAFFIC = 100 * (10 ** 6)
+PRIORITY_2_TRAFFIC = 300 * (10 ** 6)
+PRIORITY_3_TRAFFIC = 200 * (10 ** 6)
+PRIORITY_4_TRAFFIC = 100 * (10 ** 6)
 
 TOS            = 'tos'
 PRIORITY_1_TOS = 32
@@ -49,6 +49,8 @@ PRIORITY_1_DC = 1
 PRIORITY_2_DC = 1
 PRIORITY_3_DC = 1
 PRIORITY_4_DC = 1
+
+A = 1
 
 # クラスの定義
 class Device4Gateway(switch_hub.SwitchHub):
@@ -176,7 +178,6 @@ class Device4Gateway(switch_hub.SwitchHub):
         msg = ev.msg
         port = msg.match.in_port
         self.qos[port][QOS_FLAG] = QOS_OFF
-        self.dc[port] += 1
         if msg.reason == self.datapath.ofproto.OFPRR_IDLE_TIMEOUT:
             self.stats_request(self.datapath, port)
             hub.sleep(REPLY_TIME)
@@ -189,42 +190,23 @@ class Device4Gateway(switch_hub.SwitchHub):
 
     # フローをドロップ
     # フローの処理内容を空白にする
-    def drop_flow(self, port):
-        ofproto = self.datapath.ofproto
-        parser = self.datapath.ofproto_parser
-
-        match = parser.OFPMatch(in_port=port, dl_type=types.ETH_TYPE_IP, dl_dst=haddr_to_bin(ROUTER_MAC))
-        actions = []
-
-        mod = parser.OFPFlowMod(datapath=self.datapath, match=match, cookie=0, command=ofproto.OFPFC_MODIFY, actions=actions)
-
-        self.datapath.send_msg(mod)
-
-    # アドミッション制御
-    def addmission_control(self):
+    #def drop_flow(self, port):
         #ofproto = self.datapath.ofproto
         #parser = self.datapath.ofproto_parser
 
-        #actions = [parser.OFPActionOutput(ROUTER_PORT)]
+        #match = parser.OFPMatch(in_port=port, dl_type=types.ETH_TYPE_IP, dl_dst=haddr_to_bin(ROUTER_MAC))
+        #actions = []
 
-        #a = 1
+        #mod = parser.OFPFlowMod(datapath=self.datapath, match=match, cookie=0, command=ofproto.OFPFC_MODIFY, actions=actions)
+
+        #self.datapath.send_msg(mod)
+
+    # アドミッション制御
+    def addmission_control(self):
 
         switchhub = switch_hub.SwitchHub()
 
-        switchhub.drop_flow(self.datapath, haddr_to_bin(PORT4_MAC))
         switchhub.del_dropped_flow(self.datapath)
-
-        #if a > 0:
-        #    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT4_MAC))
-        #    a += 1
-        
-        #switchhub.modify_flow(self.datapath, haddr_to_bin(PORT4_MAC), ROUTER_PORT)
-        #switchhub.add_flow(self.datapath, PRIORITY_4_PORT, haddr_to_bin(PORT4_MAC), haddr_to_bin(ROUTER_PORT), actions)
-        #switchhub.del_flow(self.datapath, haddr_to_bin(PORT4_MAC))
-
-        #if a > 5:
-        #    switchhub.del_dropped_flow(self.datapath)
-        #    a = a - 5
 
         w2 = self.dc[PRIORITY_2_PORT] * (6 - PRIORITY_2_PORT)
 
@@ -233,41 +215,65 @@ class Device4Gateway(switch_hub.SwitchHub):
         w4 = self.dc[PRIORITY_4_PORT] * (6 - PRIORITY_4_PORT)
 
         # 消したフローを再登録
-        for port in self.traffic.keys():
-            if self.traffic[port] - self.qos[port][TRAFFIC] > self.base[port]:
-                if self.qos[port][QOS_FLAG] == QOS_OFF:
-                    self.add_qos(port)
+        #for port in self.traffic.keys():
+            #if self.traffic[port] - self.qos[port][TRAFFIC] > self.base[port]:
+                #if self.qos[port][QOS_FLAG] == QOS_OFF:
+                    #self.add_qos(port)
 
         # 1が通信帯域ないと
-        if self.traffic[PRIORITY_1_PORT] - self.qos[PRIORITY_1_PORT][TRAFFIC] < self.base[PRIORITY_1_PORT]:
-
-            # 2~4をドロップ
-            # dcを用いてドロップするフローを選択
-            if min([w2, w3, w4]) == w2:
-                if self.qos[PRIORITY_2_PORT][QOS_FLAG] == QOS_ON:
-                    self.drop_flow(PRIORITY_2_PORT)
-                    self.del_qos(PRIORITY_2_PORT)
-            elif min([w2, w3, w4]) == w3:
-                if self.qos[PRIORITY_3_PORT][QOS_FLAG] == QOS_ON:
-                    self.drop_flow(PRIORITY_3_PORT)
-                    self.del_qos(PRIORITY_3_PORT)
-            elif min([w2, w3, w4]) == w4:
-                if self.qos[PRIORITY_4_PORT][QOS_FLAG] == QOS_ON:
-                    #switchhub.drop_flow(self.datapath, haddr_to_bin(PORT4_MAC))
-                    #switchhub.del_flow(self.datapath, haddr_to_bin(PORT4_MAC))
-                    self.drop_flow(PRIORITY_3_PORT)
-                    self.del_qos(PRIORITY_4_PORT)
+        if self.traffic[PRIORITY_1_PORT] - self.qos[PRIORITY_1_PORT][TRAFFIC] > self.base[PRIORITY_1_PORT]:
 
             # 2が通信帯域ないと
-            if self.traffic[PRIORITY_2_PORT] - self.qos[PRIORITY_2_PORT][TRAFFIC] < self.base[PRIORITY_2_PORT]:
+            if self.traffic[PRIORITY_2_PORT] - self.qos[PRIORITY_2_PORT][TRAFFIC] > self.base[PRIORITY_2_PORT]:
 
                 # 3~4をドロップ
                 # dcを用いてドロップするフローを選択
-                if min([w3, w4]) == w3:
-                    if self.qos[PRIORITY_3_PORT][QOS_FLAG] == QOS_ON:
-                        self.drop_flow(PRIORITY_3_PORT)
-                        self.del_qos(PRIORITY_3_PORT)
-                if min([w3, w4]) == w4:
-                    if self.qos[PRIORITY_4_PORT][QOS_FLAG] == QOS_ON:
-                        self.drop_flow(PRIORITY_4_PORT)
-                        self.del_qos(PRIORITY_4_PORT)
+                if min([w4, w3]) == w3:
+                    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT3_MAC))
+                    self.dc[PRIORITY_3_PORT] += 1
+                    #if self.qos[PRIORITY_3_PORT][QOS_FLAG] == QOS_ON:
+                        #self.drop_flow(PRIORITY_3_PORT)
+                        #self.del_qos(PRIORITY_3_PORT)
+                elif min([w4, w3]) == w4:
+                    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT4_MAC))
+                    self.dc[PRIORITY_4_PORT] += 1
+                    #if self.qos[PRIORITY_4_PORT][QOS_FLAG] == QOS_ON:
+                        #self.drop_flow(PRIORITY_3_PORT)
+                        #self.del_qos(PRIORITY_4_PORT)
+
+            # 2が通信帯域あると
+            if self.traffic[PRIORITY_2_PORT] - self.qos[PRIORITY_2_PORT][TRAFFIC] < self.base[PRIORITY_2_PORT]:
+
+                # 2~4をドロップ
+                if min([w4, w3, w2]) == w2:
+                    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT2_MAC))
+                    self.dc[PRIORITY_2_PORT] += 1
+                    #if self.qos[PRIORITY_2_PORT][QOS_FLAG] == QOS_ON:
+                        #self.drop_flow(PRIORITY_2_PORT)
+                        #self.del_qos(PRIORITY_2_PORT)
+                elif min([w4, w3, w2]) == w3:
+                    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT3_MAC))
+                    self.dc[PRIORITY_3_PORT] += 1
+                    #if self.qos[PRIORITY_3_PORT][QOS_FLAG] == QOS_ON:
+                        #self.drop_flow(PRIORITY_3_PORT)
+                        #self.del_qos(PRIORITY_3_PORT)
+                elif min([w4, w3, w2]) == w4:
+                    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT4_MAC))
+                    self.dc[PRIORITY_4_PORT] += 1
+                    #if self.qos[PRIORITY_4_PORT][QOS_FLAG] == QOS_ON:
+                        #self.drop_flow(PRIORITY_3_PORT)
+                        #self.del_qos(PRIORITY_4_PORT)
+
+        # 1の通信帯域はあるが
+        elif self.traffic[PRIORITY_1_PORT] - self.qos[PRIORITY_1_PORT][TRAFFIC] < self.base[PRIORITY_1_PORT]:
+
+            # 2の通信帯域はないと
+            if self.traffic[PRIORITY_2_PORT] - self.qos[PRIORITY_2_PORT][TRAFFIC] > self.base[PRIORITY_2_PORT]:
+                
+                # 3~4をドロップ
+                if min([w4, w3]) == w3:
+                    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT3_MAC))
+                    self.dc[PRIORITY_3_PORT] += 1
+                elif min([w4, w3]) == w4:
+                    switchhub.drop_flow(self.datapath, haddr_to_bin(PORT4_MAC))
+                    self.dc[PRIORITY_4_PORT] += 1
